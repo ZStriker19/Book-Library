4f5fd14bd07f1a4904d9dc1a502264bad033e8f6
Logan Smith <logansmith0720@gmail.com>
2018-08-17 13:45:28 -0400

    Added up to date changes

--- src/main/java/com/techelevator/model/User.java
+++ src/main/java/com/techelevator/model/User.java
@@ -1,12 +1,14 @@
 package com.techelevator.model;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 public class User {
 	private String userName;
 	
-	@Size(min=10, message="Password too short, must be at least 10 characters")
 	@Pattern.List({
 		@Pattern(regexp=".*[a-z].*", message="Must have a lower case"),
 		@Pattern(regexp=".*[A-Z].*", message="Must have a capital")
