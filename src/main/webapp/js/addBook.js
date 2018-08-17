f362ca2ac82cfe50511cdab3f44cb89456885d41
Zachary Groves <zgroves19@gmail.com>
2018-08-17 11:55:38 -0400

    added js for showing forms

--- src/main/webapp/js/addBook.js
+++ src/main/webapp/js/addBook.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	$('#add-another-author')
+	let authorCounter = 1;
+	let characterCounter = 1;
+	let genreCounter = 1
+	$('#add-another-author').on('click', (event) => {
+		$('#author' + authorCounter.toString()).show('slow');
+	})
+	
+	$('#add-another-character').on('click', (event) => {
+		$('#character' + characterCounter.toString()).show('slow');
+	})
+	
+	$('#add-another-genre').on('click', (event) => {
+		$('#genre' + genreCounter.toString()).show('slow');
+	})
 })
