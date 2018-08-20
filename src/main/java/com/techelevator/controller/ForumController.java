5337eddd113dc0c162f13b47d7096c68a9eecdc6
brittduncan <brittduncan711@gmail.com>
2018-08-19 20:21:42 -0400

    forum controller

---
+++ src/main/java/com/techelevator/controller/ForumController.java
@@ -0,0 +1,35 @@
+package com.techelevator.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.techelevator.model.Book;
+import com.techelevator.model.BookDAO;
+import com.techelevator.model.User;
+import com.techelevator.model.UserDAO;
+
+@Controller
+@SessionAttributes("currentUser")
+public class ForumController {
+
+
+	@Autowired
+	private BookDAO bookDAO;
+	
+	@RequestMapping(path="/forum")
+	public String displayForum(HttpSession session, HttpServletRequest request) {
+		User currentUser = (User) session.getAttribute("currentUser");
+		
+		return "forum";
+	}
+}
